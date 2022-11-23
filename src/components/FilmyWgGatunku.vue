eb02733530070632ae1efb097d357f6e35199ee4
Interesik <interesik.pro@gmail.com>
2022-11-23 23:13:51 +0100

    przycisk hide dla komponentu 3 poprawiony na if

--- src/components/FilmyWgGatunku.vue
+++ src/components/FilmyWgGatunku.vue
@@ -1,6 +1,6 @@
 <template>
 <button @click="hidden">Pokaż/schowaj liste z gatunkami</button>
-<div v-show="hidde">
+<div v-if="hidde">
     <div v-for="(name,index) in gen" :key="index">
         <p> {{name}} </p>
         <ol>
